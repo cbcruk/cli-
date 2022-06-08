ff54e1437db39921489240bfe74cf0233d8ff403
Eunsoo Lee <cbcruk@users.noreply.github.com>
2022-06-09 05:40:42 +0900

    feat: workspace 구조로 변경

--- packages/release/fluent.js
+++ packages/release/fluent.js
@@ -3,13 +3,13 @@ const { FluentBundle, FluentResource } = require('@fluent/bundle')
 const bundle = new FluentBundle()
 const resource = new FluentResource(`
 common-pending = 요청중...
-release-type-choice1 = 
-release-type-choice2 = 
-release-type-message = 
-repo-message = 
-tag-name-message = 
-date-message = 
-confirm-message = 
+release-type-choice1 = release-type-choice1
+release-type-choice2 = release-type-choice2
+release-type-message = release-type-message
+repo-message = repo-message
+tag-name-message = tag-name-message
+date-message = date-message
+confirm-message = confirm-message
 `)
 
 bundle.addResource(resource)
