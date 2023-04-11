01c200a8f22fb3ac85572dc17715a1bdff99bd15
Eunsoo Lee <cbcruk@users.noreply.github.com>
2023-04-12 06:24:42 +0900

    update: jsx 체크 로직 변경

--- packages/git-mv-ts/src/git-mv-ts.ts
+++ packages/git-mv-ts/src/git-mv-ts.ts
@@ -1,15 +1,24 @@
 import fg from 'fast-glob'
 import { readFile } from 'fs/promises'
 import { promisify } from 'util'
+import { Parser } from 'acorn'
 
 const exec = promisify(require('child_process').exec)
+const JSXParser = Parser.extend(require('acorn-jsx')())
 
 async function main() {
   const files = await fg('./**/*.(js|jsx)')
 
   for (const file of files) {
     const data = await readFile(file, 'utf-8')
-    const isJsx = /<[a-zA-Z][^>]*>|<>/.test(data)
+    const isJsx = Boolean(
+      JSON.stringify(
+        JSXParser.parse(data, {
+          sourceType: 'module',
+          ecmaVersion: 'latest',
+        })
+      ).includes('JSXIdentifier')
+    )
 
     await exec(
       'git mv ' +
